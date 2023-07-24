d11972f052ba8bd0a00fe35e56891db1157dc530
Mauricio David Castillo Ruvalcaba <maurix.cas.ruv11@gmail.com>
2023-07-24 00:12:20 -0600

    git25

--- firebase_connection.py
+++ firebase_connection.py
@@ -2,13 +2,14 @@
 import firebase_admin
 from firebase_admin import credentials, firestore, initialize_app
 from decouple import config
+import os
 
-# Ruta al archivo de configuración de Firebase descargado
-cred = credentials.Certificate(config("JSON_FIREBASE"))
-# Inicializar la aplicación de Firebase con el nombre del bucket
-initialize_app(cred, options={
-    'storageBucket': config("BUCKET")
+firebase_credential_path = os.path.join(os.path.dirname(__file__), config("JSON_FIREBASE"))
+
+# Inicializar la aplicación de Firebase con el nombre del bucket y el proyecto
+initialize_app(credentials.Certificate(firebase_credential_path), {
+    'storageBucket': config("BUCKET"),
+    'projectId': config("PROJECT_ID")
 })
-
 # Obtén una referencia a la colección en Firestore
 db_firestore = firestore.client()

d78c1a536eb261342b1c693c6b93ed0c701676ad
Mauricio David Castillo Ruvalcaba <maurix.cas.ruv11@gmail.com>
2023-07-25 22:10:53 -0600

    cambio variable de entorno

--- firebase_connection.py
+++ firebase_connection.py
@@ -9,7 +9,7 @@ firebase_credential_path = os.path.join(os.path.dirname(__file__), config("JSON_
 # Inicializar la aplicación de Firebase con el nombre del bucket y el proyecto
 initialize_app(credentials.Certificate(firebase_credential_path), {
     'storageBucket': config("BUCKET"),
-    'projectId': config("PROJECT_ID")
+    'projectId': 'back-flutter-a83ed'
 })
 # Obtén una referencia a la colección en Firestore
 db_firestore = firestore.client()
